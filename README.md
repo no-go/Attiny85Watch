07809bded6e5fd198c42c2bc5e2e358b15df2929
jochen@homeland <no-go@localhost>
2018-02-13 13:14:00 +0100

    my actually best one

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ Use a ATtiny85 8pin AVR chip, a white LED, a 128x32 i2c oled display and 2 butto
 
 This Branch:
 
- -  the numbers of the clock is scrolling trough the display
- -  the seconds are static
- -  led on/off/flash mode (a bit strange to handle it)
+ -  runs on 8MHz instead of 1MHz (8Mhz needs more power)
+ -  displays the Lipo power in mV (menu mode)
+ -  Button 1: Display on (clock mode) or set hour/minutes
+ -  Button 2: switch through clock, led on, menu mode (set hours), menu mode (set minutes)
+
+# todo
+
+ -  test time accurency
+ -  test lifetime of 65mAh Lipo after charging it
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -6,19 +6,17 @@
 #include <avr/power.h>
 #include <avr/wdt.h>
 
-#include "FuturNumbers.h"
-using namespace FuturNumbers;
+#include "Bold.h"
+using namespace Bold;
 
-// old : tested: 14h with 1MHz 65mAh Lipo
+// try 8Mhz
 
 #define LEDPIN   1
-#define BUTTON1  4
-#define BUTTON2  3
+#define BUTTON1  3
+#define BUTTON2  4
 
-#define OFFSEC     5
-#define DELAY_QUAD 250  //should be 230 ms, but it is not (+20ms power messure delay) = 250
-#define POWERMAX   4130
-#define POWERMIN   3620
+#define OFFSEC     4
+#define DELAY_TENTH 100 // should be 100ms
 
 int hours   = 0;
 int minutes = 0;
@@ -28,23 +26,9 @@ int onsec    = 0;
 byte tick    = 0;
 byte ledon   = 0;
 
+byte menu = 0;
+
 int vcc = 3730;
-
-const byte barA1[] PROGMEM = {
-  0b11111110,
-  0b11111110
-};
-
-const byte barA2[] PROGMEM = {
-  0b11110000,
-  0b11110000
-};
-
-const byte barA3[] PROGMEM = {
-  0b00000000,
-  0b00000000
-};
-
 
 void myFont(byte x, byte b) {
   int y=0;
@@ -71,19 +55,7 @@ void myFont(byte x, byte b) {
   }
 }
 
-void scroll() {
-  ssd1306_init();
-  ssd1306_send_command(0x27); //SSD1306_LEFT_HORIZONTAL_SCROLL
-  ssd1306_send_command(0X00);
-  ssd1306_send_command(0x00); // start row
-  ssd1306_send_command(0X00);
-  ssd1306_send_command(3); // stop row
-  ssd1306_send_command(0X00);
-  ssd1306_send_command(0XFF);
-  ssd1306_send_command(0x2F); //SSD1306_ACTIVATE_SCROLL  
-}
-
-inline void bigDigital() {
+inline void readVcc() {
   // read vcc
   power_adc_enable();
   ADMUX = (0<<REFS0) | (12<<MUX0);
@@ -94,82 +66,38 @@ inline void bigDigital() {
   vcc = 1125300L / vcc;
   power_adc_disable();
 
-    
+  ssd1306_setpos(0,0);  
+  ssd1306_numdec(vcc); 
+}
+
+inline void bigDigital() {
   int t = hours/10;
-  myFont(4, t);
+  myFont(0, t);
   t = hours - t*10;
-  myFont(20, t);
+  myFont(16, t);
   
   t = minutes/10;
-  myFont(40, t);
+  myFont(36, t);
   t = minutes - t*10;
-  myFont(56, t);
+  myFont(52, t);
 
   t = seconds/10;
-  myFont(92, t);
+  myFont(77, t);
   t = seconds - t*10;
-  myFont(108, t);
-
-
-
-  if (vcc < POWERMIN) {
-    ssd1306_setpos(0,0);  
-    ssd1306_numdec(vcc); 
-  } else {
-    vcc = 8.0 * ( (float)(vcc - POWERMIN) / (float)(POWERMAX - POWERMIN) );
-    if (vcc > 7) {
-      ssd1306_draw_bmp(125, 0,  127, 1, barA1);
-      ssd1306_draw_bmp(125, 1,  127, 2, barA1);
-      ssd1306_draw_bmp(125, 2,  127, 3, barA1);
-      ssd1306_draw_bmp(125, 3,  127, 4, barA1);
-    } else if (vcc > 6) {
-      ssd1306_draw_bmp(125, 0,  127, 1, barA2);
-      ssd1306_draw_bmp(125, 1,  127, 2, barA1);
-      ssd1306_draw_bmp(125, 2,  127, 3, barA1);
-      ssd1306_draw_bmp(125, 3,  127, 4, barA1);
-    } else if (vcc > 5) {
-      ssd1306_draw_bmp(125, 0,  127, 1, barA3);
-      ssd1306_draw_bmp(125, 1,  127, 2, barA1);
-      ssd1306_draw_bmp(125, 2,  127, 3, barA1);
-      ssd1306_draw_bmp(125, 3,  127, 4, barA1);
-    } else if (vcc > 4) {
-      ssd1306_draw_bmp(125, 0,  127, 1, barA3);
-      ssd1306_draw_bmp(125, 1,  127, 2, barA2);
-      ssd1306_draw_bmp(125, 2,  127, 3, barA1);
-      ssd1306_draw_bmp(125, 3,  127, 4, barA1);
-    } else if (vcc > 3) {
-      ssd1306_draw_bmp(125, 0,  127, 1, barA3);
-      ssd1306_draw_bmp(125, 1,  127, 2, barA3);
-      ssd1306_draw_bmp(125, 2,  127, 3, barA1);
-      ssd1306_draw_bmp(125, 3,  127, 4, barA1);
-    } else if (vcc > 2) {
-      ssd1306_draw_bmp(125, 0,  127, 1, barA3);
-      ssd1306_draw_bmp(125, 1,  127, 2, barA3);
-      ssd1306_draw_bmp(125, 2,  127, 3, barA2);
-      ssd1306_draw_bmp(125, 3,  127, 4, barA1);
-    } else if (vcc > 1) {
-      ssd1306_draw_bmp(125, 0,  127, 1, barA3);
-      ssd1306_draw_bmp(125, 1,  127, 2, barA3);
-      ssd1306_draw_bmp(125, 2,  127, 3, barA3);
-      ssd1306_draw_bmp(125, 3,  127, 4, barA1);
-    } else {
-      ssd1306_draw_bmp(125, 0,  127, 1, barA3);
-      ssd1306_draw_bmp(125, 1,  127, 2, barA3);
-      ssd1306_draw_bmp(125, 2,  127, 3, barA3);
-      ssd1306_draw_bmp(125, 3,  127, 4, barA2);
-    }
-  }
+  myFont(93, t);
+  
+  myFont(111, tick);
 }
 
 inline void ticking() {
   tick++;
-  if (tick > 3) {
-    seconds += tick/4;
+  if (tick > 9) {
+    seconds += tick/10;
     if (onsec>=0 && onsec <= OFFSEC) onsec++;
   }
   
-  if (tick > 3) {
-    tick = tick % 4;
+  if (tick > 9) {
+    tick = tick % 10;
     if (seconds > 59) {
       minutes += seconds / 60;
       seconds  = seconds % 60;
@@ -193,78 +121,77 @@ void setup() {
   power_adc_disable();
   power_timer1_disable();
   
+  ssd1306_init();
   ssd1306_fill(0);
   delay(500);
-  ssd1306_setpos(0,0);
 }
 
 
 void loop() {
+  delay(DELAY_TENTH);
   ticking();
-  
-  if (onsec > OFFSEC) {
-    ssd1306_off();
-    ssd1306_send_command(0x2E); // SSD1306_DEACTIVATE_SCROLL
-    onsec = -1;
-  }
-  
-  if (ledon == 2) {
-    digitalWrite(LEDPIN, HIGH);
-    delay(50);
-    digitalWrite(LEDPIN, LOW);
-    delay(DELAY_QUAD-50);
-  } else {
-    delay(DELAY_QUAD);
-  }
 
-  // semi long press: hours up
-  // semi long press + press additional Button2: minutes up
-  if (digitalRead(BUTTON1) == LOW) {
-    onsec = 0;
-    ssd1306_on();
-    ssd1306_send_command(0x2E);
-    bigDigital();
-    scroll();
+  // --------------------------------------------------
+  if (menu==0) {
     
-    delay(500);
-    tick+=2;
+    if (onsec > OFFSEC) {
+      digitalWrite(LEDPIN, LOW);
+      ssd1306_off();
+      onsec = -1;
+    } else {
+      if (onsec >= 0) {
+        digitalWrite(LEDPIN, LOW);
+        bigDigital();
+      }
+    }
     
-    if (digitalRead(BUTTON1) == LOW) ssd1306_send_command(0x2E);
-    while (digitalRead(BUTTON1) == LOW) {
-      hours = (hours+1)%24;
-      seconds = 0;
+    if (digitalRead(BUTTON1) == LOW) {
+      onsec = 0;
+      ssd1306_on();
       bigDigital();
-      delay(200);
-    }
-    
-    if (digitalRead(BUTTON2) == LOW) ssd1306_send_command(0x2E);
-    while (digitalRead(BUTTON2) == LOW) {
-      minutes = (minutes+1)%60;
-      seconds = 0;
-      bigDigital();
-      delay(200);
-    }
-  }
-
-  // short press: Display on
-  // long press: switch LED on/off
-  if (digitalRead(BUTTON2) == LOW) {
-    onsec = 0;
-    ssd1306_on();
-    ssd1306_send_command(0x2E);
-    bigDigital();
-    scroll();
-    
-    delay(1000);
-    tick+=4;
-    if (digitalRead(BUTTON2) == LOW) {
-      ledon = (ledon+1)%3;
-    }
-    if (ledon == 1) {
-      digitalWrite(LEDPIN, HIGH);
-    } else if (ledon == 0) {
-      digitalWrite(LEDPIN, LOW);      
     } 
+
+  // --------------------------------------------------
+  } else if (menu==1) {
+    
+    digitalWrite(LEDPIN, HIGH);
+    
+  // --------------------------------------------------
+  } else if (menu==2) {
+    ssd1306_on();
+    readVcc();
+    ssd1306_string_font6x8(" mV");
+    ssd1306_setpos(0,2);
+    ssd1306_string_font6x8("set hour:  ");
+    digitalWrite(LEDPIN, LOW);
+    if (digitalRead(BUTTON1) == LOW) {
+      hours = (hours+1)%24;
+      tick=0;
+      seconds=0;
+    }
+    //ssd1306_setpos(0,2);
+    ssd1306_numdec(hours);
+    
+  // --------------------------------------------------
+  } else if (menu==3) {
+    ssd1306_on();
+    readVcc();
+    ssd1306_string_font6x8(" mV");
+    ssd1306_setpos(0,2);
+    ssd1306_string_font6x8("set minute:  ");
+    digitalWrite(LEDPIN, LOW);
+    if (digitalRead(BUTTON1) == LOW) {
+      minutes = (minutes+1)%60;
+      tick=0;
+      seconds=0;
+    }
+    //ssd1306_setpos(0,3);
+    ssd1306_numdec(minutes);
   }
 
+
+  if (digitalRead(BUTTON2) == LOW) {
+    ssd1306_fill(0);
+    menu = (menu+1)%4;
+  }
 }
